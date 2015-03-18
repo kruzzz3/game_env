a39edca1b05ecaac9979b5b48c3f3338256d2c11
Fabian Kruse <fabian.kruse@tamedia.ch>
2015-03-18 14:12:40 +0100

    correct draw line

--- core/src/ch/webk/actors/combined/Line.java
+++ core/src/ch/webk/actors/combined/Line.java
@@ -8,19 +8,8 @@ import com.badlogic.gdx.physics.box2d.Body;
 import ch.webk.box2d.SimpleUserData;
 import ch.webk.utils.ActorManager;
 import ch.webk.utils.Constants;
-import ch.webk.utils.GameMath;
-import ch.webk.utils.Logger;
-
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.physics.box2d.Body;
-
-import ch.webk.box2d.SimpleUserData;
-import ch.webk.utils.ActorManager;
-import ch.webk.utils.Constants;
 import ch.webk.utils.Logger;
 import ch.webk.utils.WorldUtils;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Line extends GameCombinedActor {
 
@@ -41,26 +30,15 @@ public class Line extends GameCombinedActor {
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        setBody(WorldUtils.createLineBody(x1, y1, x2, y2));
-        textureRegion = ActorManager.getTextureRegion(Constants.WALL);
-    }
+        float dx = x2-x1;
+        float dy = y2-y1;
+        float dist = (float) Math.sqrt(dx*dx + dy*dy);
+        Body body = WorldUtils.createLineBody(x1, y1, x2, y2);
+        body.setUserData(new SimpleUserData(dist / Constants.WORLD_TO_SCREEN, 0.1f));
+        setBody(body);
 
-    @Override
-    protected void updateRectangle() {
-        l.i("updateRectangle");
-        if (isAwake) {
-            float dx = x2-x1;
-            float dy = y2-y1;
-            float dist = (float) Math.sqrt(dx*dx + dy*dy);
-            screenRectangle.x = x1;
-            screenRectangle.y = y1;
-            screenRectangle.width = dist;
-            screenRectangle.height = 4;
-            rotDegree = (float) Math.atan2(dy, dx) * MathUtils.radiansToDegrees;
-        }
-        if (isAwake != body.isAwake()) {
-            isAwake = body.isAwake();
-        }
+        rotDegree = (float) Math.atan2(dy, dx) * MathUtils.radiansToDegrees;
+        textureRegion = ActorManager.getTextureRegion(Constants.WALL);
     }
 
     @Override
@@ -81,7 +59,7 @@ public class Line extends GameCombinedActor {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-        l.i("draw");
+        l.i("draw X="+screenRectangle.x+", Y="+screenRectangle.y+", W="+screenRectangle.width+", H="+screenRectangle.height+", R="+rotDegree);
         drawTextureRegion(batch, textureRegion,rotDegree);
     }
 }
